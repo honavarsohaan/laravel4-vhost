9cca846752d61452c7170595a25ff7e87016f7d5
honavarsohaan <honavarsohaan@gmail>
2013-04-12 20:57:33 +0530

    created workbench with laravel artisan command

---
+++ honavarsohaan/vhost/src/Honavarsohaan/Vhost/VhostServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Honavarsohaan\Vhost;
+
+use Illuminate\Support\ServiceProvider;
+
+class VhostServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
